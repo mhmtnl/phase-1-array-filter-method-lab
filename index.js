6783df6a7f098d30330024eca8d790a45084919a
mehmetunlu <munlu4037@gmail.com>
2023-05-04 19:14:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+// [1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
+// // => [4, 5]
+
+// findMatching- This function takes an array of drivers and a string as arguments, and returns the matching list of drivers. The function should be case insensitive.
+// fuzzyMatch - This function takes an array of drivers and a string as arguments for querying the array, and returns all drivers whose names begin with the provided letters.
+// matchName - This function takes an array of drivers and a string as arguments. In this function, each element of the drivers array is a JavaScript object that has a property of name. The function should return each element whose name property matches the provided string argument.
+
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase() === attribute.toLowerCase() })
+  }
+  
+  function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+  }
+  
+  function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
